e37d48e1d81126afb04e643bd32b3a209e237a48
Gabriel Hartmann <gabriel@mesosphere.io>
2016-12-01 11:59:29 -0800

    temp

---
+++ frameworks/hdfs/src/test/java/com/mesosphere/sdk/hdfs/scheduler/HelloWorldServiceSpecTest.java
@@ -0,0 +1,99 @@
+package com.mesosphere.sdk.hdfs.scheduler;
+
+import org.apache.curator.test.TestingServer;
+import org.apache.mesos.SchedulerDriver;
+import org.apache.mesos.config.ConfigStore;
+import org.apache.mesos.config.ConfigurationUpdater;
+import org.apache.mesos.offer.OfferRequirementProvider;
+import org.apache.mesos.scheduler.DefaultScheduler;
+import org.apache.mesos.specification.DefaultServiceSpec;
+import org.apache.mesos.specification.ServiceSpec;
+import org.apache.mesos.specification.yaml.YAMLServiceSpecFactory;
+import org.apache.mesos.state.StateStore;
+import org.apache.mesos.state.StateStoreCache;
+import org.junit.*;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.File;
+import java.util.Collections;
+
+import static org.apache.mesos.specification.yaml.YAMLServiceSpecFactory.generateRawSpecFromYAML;
+
+public class HelloWorldServiceSpecTest {
+    @ClassRule
+    public static final EnvironmentVariables environmentVariables = new EnvironmentVariables();
+
+    @Mock
+    private SchedulerDriver mockSchedulerDriver;
+
+    @BeforeClass
+    public static void beforeAll() {
+        environmentVariables.set("EXECUTOR_URI", "");
+        environmentVariables.set("LIBMESOS_URI", "");
+        environmentVariables.set("COUNT", "2");
+        environmentVariables.set("CPUS", "0.1");
+    }
+
+    @Before
+    public void beforeEach() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testDefaultDeserialization() throws Exception {
+        testDeserialization("svc.yml");
+    }
+
+    @Test
+    public void testDefaultValidation() throws Exception {
+        testValidation("svc.yml");
+    }
+
+    @Test
+    public void testOneTimePlanDeserialization() throws Exception {
+        testDeserialization("svc_plan.yml");
+    }
+
+    @Test
+    public void testOneTimePlanValidation() throws Exception {
+        testValidation("svc_plan.yml");
+    }
+
+    private void testDeserialization(String yamlFileName) throws Exception {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(yamlFileName).getFile());
+
+        DefaultServiceSpec serviceSpec = YAMLServiceSpecFactory
+                .generateServiceSpec(generateRawSpecFromYAML(file));
+        Assert.assertNotNull(serviceSpec);
+        Assert.assertEquals(8080, serviceSpec.getApiPort());
+        DefaultServiceSpec.getFactory(serviceSpec, Collections.emptyList());
+    }
+
+    private void testValidation(String yamlFileName) throws Exception {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(yamlFileName).getFile());
+        DefaultServiceSpec serviceSpec = YAMLServiceSpecFactory
+                .generateServiceSpec(generateRawSpecFromYAML(file));
+
+        TestingServer testingServer = new TestingServer();
+        StateStoreCache.resetInstanceForTests();
+        StateStore stateStore = DefaultScheduler.createStateStore(
+                serviceSpec,
+                testingServer.getConnectString());
+        ConfigStore<ServiceSpec> configStore = DefaultScheduler.createConfigStore(
+                serviceSpec,
+                testingServer.getConnectString(),
+                Collections.emptyList());
+
+        ConfigurationUpdater.UpdateResult configUpdateResult = DefaultScheduler
+                .updateConfig(serviceSpec, stateStore, configStore);
+
+        OfferRequirementProvider offerRequirementProvider = DefaultScheduler
+                .createOfferRequirementProvider(stateStore, configUpdateResult.targetId);
+
+        DefaultScheduler.create(serviceSpec, stateStore, configStore, offerRequirementProvider);
+    }
+}
